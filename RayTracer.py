65915db5969b9553ef6835a4e16e3f1a32d233ea
isaacdegani <isaac.degani@gmail.com>
2021-08-20 17:59:27 -0400

    now supports multiple sources

--- RayTracer.py
+++ RayTracer.py
@@ -4,14 +4,16 @@ import json
 import math
 from scipy import optimize
 from abc import ABC, abstractmethod
+# TODO: add typing, error handling
 
 
 class Path:
+    """ Collection of Rays"""
     def __init__(self, x, y, theta):
         self.rays = [[x, y, theta]]
         self.curr_ray = 0
         self.blocked = False
-        self.image = False  #  Set to true if the path makes it to the image plane
+        self.image = False  # Set to true if the path makes it to the image plane
 
     def propagate_to(self, element):
         if not self.blocked:
@@ -30,6 +32,81 @@ class Path:
 
         # show elements
         ax_sim.plot(self.rays[:, 0], self.rays[:, 1], 'ro')
+
+
+class Source(ABC):
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def get_paths(self):
+        """returns all the paths created by a source"""
+
+
+class PointSource(Source):
+    """A set of rays originating from a single point"""
+    def __init__(self, x, y, angular_i='lambertian', num_rays=15, path_to_angular_i=None):
+        super().__init__()
+        self.angular_i = angular_i
+        self.path_to_angular_i = path_to_angular_i
+        self.num_rays = num_rays
+        self.paths = []
+
+        # TODO: make this a case switch with new type
+        if angular_i == 'lambertian':
+            angularIntensity = [[]]
+        elif angular_i == 'from_path':  # Load intensity vs theta data
+            with open(self.path_to_angular_i) as f:
+                angularIntensity = json.load(f)
+        elif angular_i == 'iso':
+            angularIntensity = [[]]
+        else:
+            print('What gives?')
+
+        def get_intensity(theta, angularIntensity):
+            min_angle = angularIntensity[0][0]
+            max_angle = -min_angle
+            increment = max_angle / ((len(angularIntensity) - 1) / 2)
+            return angularIntensity[int(theta / increment + min_angle / increment)][1]
+
+        # Plot Intensity vs. theta
+        intensity_numpy = np.array(angularIntensity)
+        ax_intensity.plot(intensity_numpy[:, 0], intensity_numpy[:, 1])
+
+        # Define initial ray-thetas in density derived from angular intensity
+        theta_bins = range(-15, 15, 1)
+        initial_thetas = []
+        for theta in theta_bins:
+            rayDensity = get_intensity(theta + .5, angularIntensity)
+            num_rays = int(rayDensity * 30)
+            if num_rays > 0:
+                spacing = 1.0 / num_rays
+                for ray in range(num_rays):
+                    initial_thetas.append(math.radians(theta + ray * spacing))
+
+        # Create ray objects
+        for theta in initial_thetas:
+            self.paths.append(Path(x, y, theta))
+
+    def get_paths(self):
+        return self.paths
+
+
+class CompoundSource(Source):
+    def __init__(self, sources):
+        self.paths = list()
+        self.sources = list()
+        self.sources.extend(sources)
+        for source in self.sources:
+            self.paths.extend(source.get_paths())
+
+    def append(self, source):
+        """Add one source element to Sources"""
+        self.paths.append(source.get_paths())
+
+    def get_paths(self):
+        return self.paths
 
 
 class Element(ABC):
@@ -141,7 +218,7 @@ class LensTransition(Element):
 
 
 class Image(Element):
-    '''Image plane, defined by 2 points in 2D'''
+    """Image plane, defined by 2 points in 2D"""
     def __init__(self, x1, y1, x2, y2):
         super().__init__()
         self.x1 = x1
@@ -164,24 +241,23 @@ class Image(Element):
         return x, y
 
     def update_blocking(self, x, y, path):
-            path.blocked = True
-            """Only for image plane, update if the path impinges the image plane"""
-            # Vertical image plane
-            if (self.x2 == self.x1):
-                if self.y2 >= self.y1:
-                    if(y >= self.y1 and y <= self.y2):
-                        path.image = True
-                if self.y2 <= self.y1:
-                    if(y >= self.y2 and y <= self.y1):
-                        path.image = True
-            # Any other angle, use x to check intersection
-            if self.x2 >= self.x1:
-                if x >= self.x1 and x <= self.x2:
+        path.blocked = True
+        """Only for image plane, update if the path impinges the image plane"""
+        # Vertical image plane
+        if (self.x2 == self.x1):
+            if self.y2 >= self.y1:
+                if(y >= self.y1 and y <= self.y2):
                     path.image = True
-            if self.x1 >= self.x2:
-                if x >= self.x2 and x <= self.x1:
+            if self.y2 <= self.y1:
+                if(y >= self.y2 and y <= self.y1):
                     path.image = True
-            
+        # Any other angle, use x to check intersection
+        if self.x2 >= self.x1:
+            if x >= self.x1 and x <= self.x2:
+                path.image = True
+        if self.x1 >= self.x2:
+            if x >= self.x2 and x <= self.x1:
+                path.image = True
 
     def update_direction(self, x, y, initial_theta):
         new_theta = initial_theta
@@ -224,79 +300,64 @@ class Image(Element):
         ax_image.plot(hist_points[:, 0], hist_points[:, 1])
 
 
+class System:
+    """The collection of source, image plane, and all optical elements to simulate"""
+    def __init__(self, sources, elements, image):
+        self.sources = sources
+        self.image = image
+        self.elements = elements
+        self.elements.append(image)  # Images are just elements anyway
+
+    def run(self):
+        paths = self.sources.get_paths()  # all paths start with the source rays
+        # Calculate each ray propagating through the system
+        for path in paths:
+            for element in self.elements:
+                path.propagate_to(element)
+            path.plot()
+
+        # Plot the intensity resulting in the image plane
+        self.image.plot_image_intensity(paths)
+
+        # Show the generated simulation
+        plt.show()
+
+
 # Set up plotting
 fig_intensity, ax_intensity = plt.subplots()
 fig_sim, ax_sim = plt.subplots()
 fig_image, ax_image = plt.subplots()
+
 
 def main():
     """2D ray-tracing in simple optical system"""
 
     """**Start USER CODE**"""
     # Create system built of optical elements
+    point_source1 = PointSource(0, 0, angular_i='from_path', path_to_angular_i='AngularIntensity.json')
+    point_source2 = PointSource(0, 10, angular_i='from_path', path_to_angular_i='AngularIntensity.json')
+    compound_source = CompoundSource([point_source1, point_source2])
+
     lens_thickness = 2.5
     lens_start_distance = 29.1
     lens_start = LensTransition(lens_start_distance, 0, 1/4.01)
     lens_end = LensTransition(lens_start_distance + lens_thickness, -45.1, 4.01/1)
 
-    
-    #aperture_start = Aperture(33, -1.4, 1.4)
-    #aperture_end = Aperture(34, -1.4, 1.4)
-    
+    aperture_start = Aperture(33, -1.4, 1.4)
+    aperture_end = Aperture(34, -1.4, 1.4)
+
     """Bed extents"""
     bed_center_distance = 343.9
     bed_len = 165
     bed_theta = math.radians(90 - 19.83)
-    bed_x1, bed_y1 = bed_center_distance - math.cos(bed_theta)*bed_len/2, -bed_len/2*math.sin(bed_theta) 
+    bed_x1, bed_y1 = bed_center_distance - math.cos(bed_theta)*bed_len/2, -bed_len/2*math.sin(bed_theta)
     bed_x2, bed_y2 = bed_center_distance + math.cos(bed_theta)*bed_len/2, bed_len/2*math.sin(bed_theta)
-    """"""
-    bed  = Image(bed_x1, bed_y1, bed_x2, bed_y2)
-    
-    #System = [bed]
-    System = [lens_start, lens_end, bed]
-    #System = [lens_start, lens_end, aperture_start,aperture_end, bed]
+
+    bed = Image(bed_x1, bed_y1, bed_x2, bed_y2)
+
+    system = System(compound_source, [lens_start, lens_end, aperture_start, aperture_end], bed)
+    system.run()
     """**END USER CODE**"""
-    
-    # Load intensity vs theta data
-    with open('AngularIntensity.json') as f:
-        angularIntensity = json.load(f)
-
-    def get_intensity(theta, angularIntensity):
-        min_angle = angularIntensity[0][0]
-        max_angle = -min_angle
-        increment = max_angle / ((len(angularIntensity) - 1) / 2)
-        return angularIntensity[int(theta / increment + min_angle / increment)][1]
-    # Plot Intensity vs. theta
-    intensity_numpy = np.array(angularIntensity)
-    ax_intensity.plot(intensity_numpy[:, 0], intensity_numpy[:, 1])
-
-    # Define initial ray-thetas in density derived from angular intensity
-    theta_bins = range(-15, 15, 1)
-    initial_thetas = []
-    for theta in theta_bins:
-        rayDensity = get_intensity(theta + .5, angularIntensity)
-        num_rays = int(rayDensity * 30)
-        if num_rays > 0:
-            spacing = 1.0 / num_rays
-            for ray in range(num_rays):
-                initial_thetas.append(math.radians(theta + ray * spacing))
-
-    # Create ray objects
-    paths = []
-    for theta in initial_thetas:
-        paths.append(Path(0, 0, theta))
-
-    # Here we calculate each ray propagating through the system
-    for path in paths:
-        for element in System:
-            path.propagate_to(element)
-        path.plot()
-
-    # Plot the intensity resulting in the image plane
-    bed.plot_image_intensity(paths)
-
-    # Show the generated simulation
-    plt.show()
 
 
 if __name__ == "__main__":
